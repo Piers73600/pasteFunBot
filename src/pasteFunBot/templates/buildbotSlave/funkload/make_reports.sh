1fc7b896e91137562a765e5d83e51300847c2e03
Makina-stpda <stpda@stpda-laptop.(none)>
2009-06-29 16:02:18 +0200

    27

--- src/pasteFunBot/templates/buildbotSlave/funkload/make_reports.sh
+++ src/pasteFunBot/templates/buildbotSlave/funkload/make_reports.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd $1
+cd $1/xml/
 fl-build-report --html --output-directory=../reports/ $2
 sleep 5
-cd ../reports/
+cd ../../reports/
 current=`ls | grep -v diff | tail -1`
 last=`ls | grep -v diff | tail -2 | head -1`
 fl-build-report --diff $last $current
