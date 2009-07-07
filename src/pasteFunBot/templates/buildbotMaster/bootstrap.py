2f4a70f2a475bc3515ba62147ad83a9d14aa59da
Makina-stpda <stpda@stpda-laptop.(none)>
2009-07-07 17:01:50 +0200

    pkg_resources

--- src/pasteFunBot/templates/buildbotMaster/bootstrap.py
+++ src/pasteFunBot/templates/buildbotMaster/bootstrap.py
@@ -28,7 +28,7 @@ exec urllib2.urlopen('http://peak.telecommunity.com/dist/ez_setup.py'
                      ).read() in ez
 ez['use_setuptools'](to_dir=tmpeggs, download_delay=0)
 
-import pkg_ressources
+import pkg_resources
 
 cmd = 'from setuptools.command.easy_install import main; main()'
 if sys.platform == 'win32':
